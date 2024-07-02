59fcd94c5f43ec5834192ea716df3227dabb2e8b
nev <165979572+fadedgrime@users.noreply.github.com>
2024-07-02 01:14:01 -0600

    Updated text

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Integrity Holdings Chief Executive Officer",
+          "Certified Nursing Assistant",
+          "Soon to be Web Developer",
+          "Entrepreneur",
         ],
         autoStart: true,
         loop: true,
